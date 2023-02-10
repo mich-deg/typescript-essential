0015c8f6fe3c110b040fa7db4a9df08877c38101
Michael <lemichael1215@gmail.com>
2023-02-10 17:48:39 +0300

    added interface

--- src/demo.ts
+++ src/demo.ts
@@ -1,5 +1,17 @@
-class Contact {
-  id;
-  name;
-  birthDate;
+// class Contact {
+//   id;
+//   name;
+//   birthDate;
+// }
+
+interface Contact {
+  id: number;
+  name: string;
+  birthDate?: Date;
 }
+
+let primaryContact: Contact = {
+  id: 12345,
+  name: "Michael Johnson",
+  birthDate: new Date("12-12-1990"),
+};
