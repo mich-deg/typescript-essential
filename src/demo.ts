87c62179d80077d54b328d05061916aa583fe7aa
Michael <lemichael1215@gmail.com>
2023-02-10 17:52:15 +0300

    merging custom types with interfaces

--- src/demo.ts
+++ src/demo.ts
@@ -4,14 +4,24 @@
 //   birthDate;
 // }
 
-interface Contact {
+interface Contact extends Address {
   id: number;
   name: string;
   birthDate?: Date;
+}
+
+interface Address {
+  line1: string;
+  line2?: string;
+  province?: string;
+  region?: string;
+  postalCode: string;
 }
 
 let primaryContact: Contact = {
   id: 12345,
   name: "Michael Johnson",
   birthDate: new Date("12-12-1990"),
+  postalCode: "1000",
+  line1: "addis 100",
 };
