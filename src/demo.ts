c2fbd77578f995ae79b4d47045bc9cd3c389b5f8
Michael <lemichael1215@gmail.com>
2023-02-11 14:11:36 +0300

    added Partial type

--- src/demo.ts
+++ src/demo.ts
@@ -113,3 +113,23 @@ interface ContactEvent {
 // defining dynamic but limited types with records
 let X: Record<string, string | number> = { name: "Wruce Bayne" };
 X.number = 1234;
+
+// Partial type - constructs a type with all properties of Type set to optional.
+
+interface Todo {
+  title: string;
+  description: string;
+}
+
+function updateTodo(todo: Todo, fieldsToUpdate: Partial<Todo>) {
+  return { ...todo, ...fieldsToUpdate };
+}
+
+const todo1 = {
+  title: "organize desk",
+  description: "clear clutter",
+};
+
+const todo2 = updateTodo(todo1, {
+  description: "throw out trash",
+});
