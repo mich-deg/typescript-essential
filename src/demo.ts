e9ca8b3c01e4a64d120cd5f426e51e867ccb0d7d
Michael <lemichael1215@gmail.com>
2023-02-12 15:36:27 +0300

    added Required Utility Type

--- src/demo.ts
+++ src/demo.ts
@@ -165,3 +165,13 @@ const todoP: TodoPreviewPick = {
   title: "Clean room",
   completed: false,
 };
+
+// Required - constructs a type consisting of all properties of Type set to required. The opposite of Partial.
+interface Props {
+  a?: number;
+  b?: string;
+}
+
+const obj: Props = { a: 5 };
+
+const obj2: Required<Props> = { a: 5, b: "foo" };
